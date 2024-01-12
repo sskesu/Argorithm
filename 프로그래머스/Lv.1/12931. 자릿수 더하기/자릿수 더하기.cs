5ed0e022dbc1b0eba2c9e1466140810484fbce8d
sskesu <136444697+sskesu@users.noreply.github.com>
2024-01-12 09:39:30 +0900

    [level Lv.1] Title: 자릿수 더하기, Time: 0.17 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/12931. 자릿수 더하기/자릿수 더하기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        while(n >= 10)
+        {
+           answer += n % 10;
+            n /= 10;
+        }
+        answer += n;
+        return answer;
+    }
+}
