d9e5cf8d5e05997e55dfafbe14af1fd1c8a3805b
sskesu <136444697+sskesu@users.noreply.github.com>
2024-01-16 09:31:00 +0900

    [level Lv.1] Title: 콜라츠 추측, Time: 0.18 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/12943. 콜라츠 추측/콜라츠 추측.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long n = (long)num;
+        while (answer < 500)
+        {
+            if (n == 1)
+            {
+                return answer;
+            }
+            else if (n % 2 == 0)
+            {
+               n /= 2;
+            }
+            else
+            {
+                n = n * 3 + 1;
+            }
+            answer++;
+        }
+        return -1;
+    }
+}
