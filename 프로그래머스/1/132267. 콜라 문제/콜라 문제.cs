4956c0151530930d98e230831adee5a3e36c5029
sskesu <136444697+sskesu@users.noreply.github.com>
2024-02-15 09:21:09 +0900

    [level 1] Title: 콜라 문제, Time: 0.17 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        while (n >= a)
+        {
+            n -= a;
+            answer += b;
+            n += b;
+        }
+        return answer;
+    }
+}
