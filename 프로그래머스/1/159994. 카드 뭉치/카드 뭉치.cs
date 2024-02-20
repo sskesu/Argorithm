5b401451b82e2cecc87727493b873095c40a349c
sskesu <136444697+sskesu@users.noreply.github.com>
2024-02-20 10:22:52 +0900

    [level 1] Title: 카드 뭉치, Time: 0.21 ms, Memory: 30.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cs
@@ -0,0 +1,32 @@
+using System;
+
+public class Solution {
+    public string solution(string[] cards1, string[] cards2, string[] goal) {
+        int index1 = 0;
+        int index2 = 0;
+        for (int i = 0; i < goal.Length; i++)
+        {
+            if (index1 < cards1.Length)
+            {
+                if (goal[i] == cards1[index1])
+                {
+                    index1++;
+                    continue;
+                }
+            }
+            
+            if (index2 < cards2.Length)
+            {
+                if (goal[i] == cards2[index2])
+                {
+                    index2++;
+                    continue;
+                }
+            }
+
+            return "No";
+            
+        }
+        return "Yes";
+    }
+}
