aa556702111412eb2470d5f346a9556d44939519
sskesu <136444697+sskesu@users.noreply.github.com>
2024-03-06 22:54:34 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.81 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cs
@@ -0,0 +1,27 @@
+using System;
+
+public class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        for (int i = 1; i <= number; i++)
+        {
+            int count = 1;
+            for (int j = 1; j <= i / 2; j++)
+            {
+                if (i % j == 0)
+                {
+                    count++;
+                }
+            }
+            if (count > limit)
+            {
+                answer += power;
+            }
+            else
+            {
+                answer += count;
+            }
+        }
+        return answer;
+    }
+}
